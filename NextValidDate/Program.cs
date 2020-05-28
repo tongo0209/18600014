3c7e6813a912a8e46d26eea6312a5a3cd14a611d
tongo0209 <tongo0209@gmail.com>
2020-05-28 11:53:01 +0700

    Add project files.

---
+++ NextValidDate/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace NextValidDate
+{
+    class Program
+    {
+
+        static bool CheckYear(int year)
+        {
+            return(year%4==0 && year%100 !=-0) ||(year%400==0);
+        }
+		static int DayMonth(int month, int year)
+		{
+			switch (month)
+			{
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+				case 8:
+				case 10:
+				case 12:
+					return 31;
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					return 30;
+					break;
+				case 2:
+					if (CheckYear(year) == true)
+						return 29;
+					else
+						return 28;
+
+				default:
+					return 0;
+			}
+				
+		}
+		static void NextDay(int day, int month, int year)
+		{
+			int DayinMonth = DayMonth(month, year);
+			if (day < DayinMonth) 
+			{
+				day++;
+			}
+			else if (month < 12)     
+			{
+				day = 1; month++;
+			}
+			else                     
+			{
+				day = month = 1;
+				year++;
+			}
+			Console.WriteLine($"Ngay ke tiep la: {day}/{month}/{year} ");
+		}
+		static void Main(string[] args)
+        {
+			Console.Write("Nhap day: ");
+			int day = int.Parse(Console.ReadLine());
+			Console.Write("Nhap thang: ");
+			int month = int.Parse(Console.ReadLine());
+			Console.Write("Nhap nam: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine(day + "/" + month + "/"+ year);
+			int DayInMonth = DayMonth(month, year);
+			NextDay(day, month, year);
+        }      
+    }
+}
---
+++ QuickImage/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void click(object sender, RoutedEventArgs e)
+        {
+            var rng = new Random();
+            string[] quotes = {"Tiếp tục đi. Tất cả mọi thứ bạn cần sẽ đến với bạn vào thời điểm hoàn hảo.",
+                "You have to be at your strongest when you’re feeling at your weakest.",
+                "Nhìn vào gương. Đó là sự cạnh tranh của bạn.",
+                "Tập trung vào mục tiêu của bạn. Đừng nhìn theo bất kỳ hướng nào ngoài phía trước.",
+                "Đau là tạm thời. Thoát. - Lance Armstrong",
+                "Chúng ta phải nắm lấy nỗi đau và đốt nó làm nhiên liệu cho cuộc hành trình của chúng ta. - Kenji Miyji",
+                "Một vấn đề là một cơ hội để bạn cố gắng hết sức. - Công tước Ellington",
+                "Hãy nhớ rằng đó chỉ là một ngày tồi tệ, không phải là một cuộc sống tồi tệ.",
+                "Nó không hoàn hảo. Đó là về nỗ lực. - Jillian Michaels",
+                "Hãy tin rằng bạn có thể và bạn đang ở giữa chừng. - Theodore Roosevelt"
+    };
+            int i = rng.Next(quotes.Length);
+            string quote = quotes[i];
+
+            MessageBox.Show(quote); ;
+        }
+
+        private void image_Click(object sender, RoutedEventArgs e)
+        {
+            string[] quotes = {"Tiếp tục đi. Tất cả mọi thứ bạn cần sẽ đến với bạn vào thời điểm hoàn hảo.",
+                "You have to be at your strongest when you’re feeling at your weakest.",
+                "Nhìn vào gương. Đó là sự cạnh tranh của bạn.",
+                "Tập trung vào mục tiêu của bạn. Đừng nhìn theo bất kỳ hướng nào ngoài phía trước.",
+                "Đau là tạm thời. Thoát. - Lance Armstrong",
+                "Chúng ta phải nắm lấy nỗi đau và đốt nó làm nhiên liệu cho cuộc hành trình của chúng ta. - Kenji Miyji",
+                "Một vấn đề là một cơ hội để bạn cố gắng hết sức. - Công tước Ellington",
+                "Hãy nhớ rằng đó chỉ là một ngày tồi tệ, không phải là một cuộc sống tồi tệ.",
+                "Nó không hoàn hảo. Đó là về nỗ lực. - Jillian Michaels",
+                "Hãy tin rằng bạn có thể và bạn đang ở giữa chừng. - Theodore Roosevelt";
+            Random img = new Random();
+            int ran = img.Next(0, 10);
+            var uri = new Uri("/Image", UriKind.Relative);
+            bao.Source = new BitmapImage(uri);
+        }
+    }
+}
